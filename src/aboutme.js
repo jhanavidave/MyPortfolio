1c5023c9c2828c61270bbe35860e285026fec2ae
Jhanavi <jhanavi.dave97@gmail.com>
2024-10-22 12:33:55 -0500

    Sixth Commit - please let this fix the back page issue

---
+++ src/aboutme.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Aboutme() {
+  return (
+    <div>
+      <hr color='black' height='1.5px' />
+      <p>Here is my about me content.
+        who am i?<br />
+        strengths<br />
+        weakenesses<br />
+        interests<br />
+        socials - add flipcards as icons and hyperlink
+      </p>
+    </div>
+  );
+}
+
+export default Aboutme;
---
+++ src/profex.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+function Profex() {
+  return (
+    <div>
+      <hr color='black' height='1.5px' />
+      <p id="spacing">
+        <b><u>Company:</u></b> Colgate Global Business Services Private Limited
+        <br />
+        <b><u>Location:</u></b> Mumbai, MH, India
+        <br />
+        <b><u>Tenure:</u></b> JUL. 2019 - JUN. 2022
+        <br />
+        <b><u>Role:</u></b> Senior Technology Associate
+        <br />
+        
+        <h2><b><u><center>RESPONSIBILITIES:</center></u></b></h2>
+        <ul>
+          <li>Delivered robust solutions by implementing technical changes and configurations, utilizing cutting-edge technologies, and resolving issues through agile troubleshooting and proactive updates.</li>
+          <li>Conducted in-depth analysis of business requirements, leveraging advanced analytical skills to identify challenges and opportunities, while delivering actionable insights and strategic guidance to business teams.</li>
+          <li>Collaborated with project teams to define scope, set development timelines, and create detailed roll-out plans, ensuring strict adherence to high-quality development standards and deadlines.</li>
+          <li>Ensured timely and accurate project delivery by adhering to development standards, processes, and timelines, while establishing metrics and benchmarks to track performance against business standards.</li>
+          <li>Engaged in regular code reviews, providing constructive feedback to ensure high code quality and adherence to best practices.</li>
+        </ul>
+
+        <h2><b><u><center>PROJECTS:</center></u></b></h2>
+
+        <b>AUTOMATED JOB SHEET:</b>
+        <br />
+        AUG. 2019 - NOV. 2019
+        <ul>
+          <li>Technologies used: Google Sheets, JavaScript, Google App Script, RestAPI, Google API, HTML5/CSS3, Java</li>
+          <li>Self-initiated and developed an automated monitoring system using Google App Script for approximately 30k daily SAP jobs. The system tracked job completion times, notified the team of delays or failures, saving 30-45 minutes of manual monitoring daily, and boosting overall efficiency by 3x monthly while reducing incident tickets by 15%.</li>
+        </ul>
+
+        <b>ASIA PACIFIC DEMAND SENSING ROLLOUT:</b>
+        <br />
+        JUN. 2020 - DEC. 2020
+        <ul>
+          <li>Technologies used: Demand Planning Algorithms, SAP, Inventory Optimization Tools</li>
+          <li>Implemented a demand sensing algorithm in the Asia-Pacific production region to automatically calculate forecasts for the immediate 6 weeks using future baseline, sales orders, shipments, and uplifts. This increased precision in planning, inventory optimization, and deployment.</li>
+        </ul>
+
+        <b>NORTH AMERICA S/4HANA CONSOLIDATION:</b>
+        <br />
+        JUN. 2020 – APR. 2021
+        <ul>
+          <li>Technologies used: SAP R/3, SAP S/4HANA, IBP (Integrated Business Planning)</li>
+          <li>Combined two North American SAP R/3 Production systems into a single, consolidated SAP S/4HANA system, ensuring continued business processes with high data storage and transmission capabilities. Monitored SAP Extraction and IBP job change levels, addressing issues and failures during the transition.</li>
+        </ul>
+
+        <b>PROCESS AUTOMATION FOR PRODUCTION SYSTEM ITEM MANAGEMENT:</b>
+        <br />
+        AUG. 2021 - MAR. 2022
+        <ul>
+          <li>Technologies used: UiPath, SAP Production Systems, MRP Controllers</li>
+          <li>Automated the creation, extension, and regeneration of timestreams, MRP Controllers, and Cascade Lane Setup using UiPath in global Production Systems. Implemented secure login and item management automation, saving over 200 hours of manual work monthly and significantly enhancing operational efficiency.</li>
+        </ul>
+
+        <b>YEAR-END ACTIVITIES AUTOMATION:</b>
+        <br />
+        NOV. 2021 - JAN. 2022
+        <ul>
+          <li>Technologies used: UiPath, SAP, Calendar Management Tools</li>
+          <li>Led the automation of critical year-end activities in global Production Systems, including fiscal year changes, planning area extensions, calendar synchronization, and time stream regeneration. This automation reduced daily workload by 5 tickets, streamlining year-end processes.</li>
+        </ul>
+
+      </p>
+    </div>
+  );
+}
+
+export default Profex;
